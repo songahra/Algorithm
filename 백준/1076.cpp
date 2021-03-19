e5c85f34a196a10137afc90f4a6ed03bc6721257
2041420 <a2041420@gmail.com>
2021-03-19 18:00:02 +0900

    commit

---
+++ 백준/1076.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(){
+    map<string,pair<int,int>> d1={
+        {"black",{0,1}},{"brown",{1,10},},{"red",{2,100}},{"orange",{3,1000}},{"yellow",{4,10000}},
+        {"green",{5,100000}},{"blue",{6,1000000}},{"violet",{7,10000000}},{"grey",{8,100000000}}
+    };
+    string str1;
+    string str2;
+    string str3;
+
+
+    cin >> str1 >> str2 >> str3;
+
+    return 0;
+}
---
+++ 백준/2743.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main(){
+    string str;
+    cin >> str;
+
+    cout << str.size();
+
+    return 0;
+}
